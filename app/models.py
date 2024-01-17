e1d517a203cb4f5f8868535d27c992b58e273e68
FaithAdline <144058241+FaithAdline@users.noreply.github.com>
2024-01-17 09:37:44 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,14 +1,29 @@
-# models.py
-
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
-class Hero(db.Model):
-    __tablename__ = 'heroes'
 
+hero_power_association = db.Table('hero_power_association',
+    db.Column('hero_id', db.Integer, db.ForeignKey('hero.id')),
+    db.Column('power_id', db.Integer, db.ForeignKey('power.id')),
+    db.Column('Strength', db.String, ('Strong'),('Weak'),('Average'))
+)
+
+
+class Heros(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    super_name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255))
+    powers = db.relationship('Power', secondary=hero_power_association, back_populates='heroes')
 
-# Add similar primary key definitions for other models (Power, HeroPower) as needed
+class Powers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    heroes = db.relationship('Hero', secondary=hero_power_association, back_populates='powers')
+    description = db.Column(db.String,('description >= 20'),nullable = False)
+
+db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
